c1b02ff41db4b9697e4f5451287adad761362c9b
Jose Galicia <crasho1481@gmail.com>
2024-10-20 16:58:05 -0500

    refactor totalPerPerson widget

--- lib/main.dart
+++ lib/main.dart
@@ -65,24 +65,7 @@ class _UTipState extends State<UTip> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-                padding: const EdgeInsets.all(18),
-                decoration: BoxDecoration(
-                  color: theme.colorScheme.inversePrimary,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  children: [
-                    Text("Total per Person", style: style),
-                    Text("\$${_counterService.totalPerPerson().toStringAsFixed(2)}",
-                        style: style.copyWith(
-                            color: theme.colorScheme.onPrimary,
-                            fontSize: theme.textTheme.displaySmall?.fontSize)),
-                  ],
-                )),
-          ),
+          TotalPerPerson(theme: theme, style: style, counterService: _counterService),
           // Form
           Padding(
             padding: const EdgeInsets.all(8.0),
